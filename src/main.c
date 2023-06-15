f89f4ffcd997be653ff7920ebb11b29ec0c16795
Jisse Meruma <jissemeruma@gmail.com>
2023-06-15 13:03:36 +0200

    First draft of the most amazing cub3d u will ever see :D

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+#include "MLX42.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error invalid arguments: Usage: ./cub3d + ANOTHER ARGUMENT stoopid..");
+		return (EXIT_FAILURE);
+	}
+	
+}
